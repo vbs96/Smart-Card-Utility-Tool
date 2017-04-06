2e5cc53b38b9cdc3f06a16758e79975caaa8d0f2
Vitelaru Bogdan <vitelarubogdan96@gmail.com>
2017-04-06 20:38:33 +0300

    Update Instruction.h

--- Instruction.h
+++ Instruction.h
@@ -6,10 +6,11 @@ class Instruction
 	unsigned char* standardInstruction;
 	int len;
 
-public:
-	Instruction(const unsigned char* stdInstruction,int len);
 	LPCBYTE Append(const unsigned char* toAdd,int len);
 	unsigned char* GetInstr() { return standardInstruction; }
+	//eventual poti sa faci chestia aia cu private implementation 
+public:
+	Instruction(const unsigned char* stdInstruction,int len);
 	~Instruction();
 
 };
